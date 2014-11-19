23dc2bcf0d96fed27c369b4f5b0c8c0c1ed4e98c
philmphoenix <phillip.matthew.phoenix@gmail.com>
2014-11-19 16:19:28 -0700

    Tried removing migration that may have been the issue

--- db/migrate/20141114223543_add_more_columns_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddMoreColumnsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :last_name, :string
-    add_column :users, :description, :texts
-    add_column :users, :headline, :string
-  end
-end
